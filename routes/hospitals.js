593e60c074c0d59eb37beb060a9fe88ba1351a42
Pattadon Prongpreecha <pattadon.pro@gmail.com>
2023-02-14 23:33:19 +0700

    Apply authentication

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -7,8 +7,9 @@ const {
   updateHospital,
   deleteHospital,
 } = require("../controllers/hospital");
+const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(getHospitals).post(createHospital);
-router.route("/:id").get(getHospital).put(updateHospital).delete(deleteHospital);
+router.route("/").get(getHospitals).post(protect, authorize("admin"), createHospital);
+router.route("/:id").get(getHospital).put(protect, authorize("admin"), updateHospital).delete(protect, authorize("admin"), deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+const cookieParser = require("cookie-parser");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -10,8 +12,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
