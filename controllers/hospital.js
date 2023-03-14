a1a17ebea82fd424d90617257fff01dc2c7ac613
Pattadon Prongpreecha <pattadon.pro@gmail.com>
2023-03-14 21:11:36 +0700

    Update controller

--- controllers/hospital.js
+++ controllers/hospital.js
@@ -2,9 +2,63 @@ const Hospital = require("../models/hospital");
 
 exports.getHospitals = async (req, res, next) => {
   try {
-    const hospitals = await Hospital.find();
+    let query;
+    const reqQuery = {...req.query};
+
+    // fields to exclude
+    const removeFields = ["select", "sort", "page", "limit"];
+    removeFields.forEach(param => delete reqQuery[param]);
+
+    let queryStr = JSON.stringify(reqQuery);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    //finding resource
+    query = Hospital.find(JSON.parse(queryStr));
+
+    //select fields
+    if(req.query.select) {
+      const fields = req.query.select.split(",").join(" ");
+      query = query.select(fields);
+    }
+
+    //sort
+    if(req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const total = await Hospital.countDocuments();
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    query = query.skip(startIndex).limit(limit);
+
+    //executing query
+    const hospitals = await query;
+
+    //pagination result
+    const pagination = {};
+    if(endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit
+      }
+    }
+
+    if(startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit
+      }
+    }
+
     res.status(200).json({
       success: true,
+      count: hospitals.length,
       data: hospitals,
     });
   } catch (err) {
